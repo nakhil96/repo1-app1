952220713843bb2fa7dcd30c9183845f6f6192b2
nakhil96 <akhil.nalumachu@gmail.com>
2018-04-14 10:01:09 +0530

    committed new properties

---
+++ .DevPipeline/.DevAppProperties.groovy
@@ -0,0 +1,30 @@
+#This File conatins the properties that are common to the pipeline functioning
+
+           releaseNumber =  "R2017.03.1"
+           prop2_url = "Tools_Home" + "/opt/apa/exa"
+	   #a_comment = "This ia a comment and these shouldn't be considered while scanning the file"            
+	   prop3  =   "abc"
+	   prop4  =   "xyz"
+	   envrionments = [][]
+
+#Along with these properties there will also be getter and setter functions that should be ignored
+
+
+get environments{
+some x;
+}
+
+
+get someOtherProp{
+some y;
+}
+
+
+set prop3{
+some z; 
+}
+
+
+
+
+
