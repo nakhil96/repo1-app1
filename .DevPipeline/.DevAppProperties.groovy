45b3b4a388ad186b7c36f182c9e68cafe63de081
nakhil96 <akhil.nalumachu@gmail.com>
2018-04-14 19:53:44 +0530

    Added New properties

---
+++ .DevPipeline/.DevAppProperties.groovy
@@ -0,0 +1,28 @@
+#This File conatins the properties that are common to the pipeline functioning
+
+           releaseNumber =  "R2017.03.1"
+    	   #a_comment = "This ia a comment and these shouldn't be considered while scanning the file"            
+   	   prop3  =   "abc"
+   	   prop4  =   "xyz"
+  	   envrionments = [][]
+
+#Along with these properties there will also be getter and setter functions that should be ignored
+ get environments{
+ some x;
+}
+
+
+ get someOtherProp{
+  some y;
+ }
+
+
+ set prop3{
+ some z; 
+ }
+
+
+
+
+
+
---
+++ .DevPipeline/.Env2Properties.groovy
@@ -0,0 +1,13 @@
+#This file also conatins some properties that are similar to DevAppProperties but specific to differnt environments
+
+            prop1_requires =  "x"
+	    prop2_requeired =  "y"       
+	    prop3_requires   = "z"
+	    #prop4  = "a" + "abc.xyx.xds"
+
+
+#Please make sure tat you ignore the comments	    
+
+
+
+
